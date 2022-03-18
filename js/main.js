e68e5c8dda8d6e1a2a87b51766db031e6bb08eba
alrebellato <74402509+alrebellato@users.noreply.github.com>
2022-03-18 16:51:46 +1100

    implemented djikstra distances

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ var canvasHeight = canvas.height;
 
 
 //Map Size
-var mapColumns = 96;
-var mapRows = 54;
+var mapColumns = 20;
+var mapRows = Math.floor(mapColumns*9/16);
 
 //Wall Placement
 var smoothness = 2;
@@ -17,8 +17,8 @@ var WallPercentChance = 42;
 var tileOuterSize = canvasWidth / mapColumns
 var tileSize = (canvasWidth / mapColumns)-1;
 
-var floor = 0;
-var wall = 1;
+const FLOOR = 0;
+const WALL = 1;
 
 var wallColour = "black";
 var floorColour = "gainsboro"
@@ -48,21 +48,41 @@ function drawEntities(entityMap){
 
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    //TODO print based on priority
+    //Prints only if higher priority than previous entity
+
+    knownPositions = new Map();
+
 
     for(var e in entityMap){
+        entity = entityMap[e]
+        higherPriority = false
 
-        ctx.beginPath();
-        ctx.rect(entityMap[e].position[1] * tileOuterSize + 1, 
-                 entityMap[e].position[0] * tileOuterSize + 1, 
-                 tileSize, tileSize);
-        ctx.fillStyle = entityMap[e].colour;
-        ctx.fill();
-        ctx.closePath();
+        if(knownPositions.has(entity.pos)){
+            if(entity.priority > knownPositions[entity.pos]){
+                knownPositions[entity.pos] = entity.priority;
+                higherPriority = true;
+            }
+        }
+        else{
+            knownPositions[entity.pos] = entity.priority;
+            higherPriority = true;
+        }
+
+
+        if(higherPriority){
+            ctx.beginPath();
+            ctx.rect(entity.position[1] * tileOuterSize + 1, 
+                     entity.position[0] * tileOuterSize + 1, 
+                     tileSize, tileSize);
+            ctx.fillStyle = entity.colour;
+            ctx.fill();
+            ctx.closePath();
+        }
+
     }
 }
 
-function makeMap(entityMap, smoothness) {
+function makeMap(smoothness) {
     map = Array(mapRows).fill(0).map(x => Array(mapColumns).fill(0))
     generateNoise(map);
     addMapBorder(map);
@@ -78,7 +98,7 @@ function generateNoise(inputArray) {
     for(let i=0; i < mapRows; i++){
         for(let j=0; j < mapColumns; j++){
             if(Math.floor(Math.random() * 100) < WallPercentChance){
-                inputArray[i][j] = wall;
+                inputArray[i][j] = WALL;
             }
         }
     }
@@ -88,11 +108,11 @@ function addMapBorder(inputMap){
     //Populates the edge of the map with walls
     for(let i=0; i < mapColumns; i++){
         inputMap[0][i] = 1;
-        inputMap[mapRows-1][i] = wall;
+        inputMap[mapRows-1][i] = WALL;
     }
     for(let i=0; i < mapRows; i++){
         inputMap[i][0] = 1;
-        inputMap[i][mapColumns-1] = wall;
+        inputMap[i][mapColumns-1] = WALL;
     }
 }
 
@@ -114,36 +134,36 @@ function smooothMap(inputMap){
             //if currently a wall, neighborCount = 1, else 0.
             let neighborCount = inputMap[i][j];
 
-            if(inputMap[i-1][j-1] == wall){
+            if(inputMap[i-1][j-1] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i-1][j] == wall){
+            if(inputMap[i-1][j] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i-1][j+1] == wall){
+            if(inputMap[i-1][j+1] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i][j+1] == wall){
+            if(inputMap[i][j+1] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i+1][j+1] == wall){
+            if(inputMap[i+1][j+1] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i+1][j] == wall){
+            if(inputMap[i+1][j] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i+1][j-1] == wall){
+            if(inputMap[i+1][j-1] == WALL){
                 neighborCount += 1;
             }
-            if(inputMap[i][j-1] == wall){
+            if(inputMap[i][j-1] == WALL){
                 neighborCount += 1;
             }
 
             if(neighborCount >= 5){
-                tempArray[i][j] = wall;
+                tempArray[i][j] = WALL;
             }
             else{
-                tempArray[i][j] = floor;
+                tempArray[i][j] = FLOOR;
             }
         }
     }
@@ -168,13 +188,104 @@ function numsToEnts(inputMap){
     return entityMap;
 }
 
+function buildLevel(smoothness){
+    entities = new Map();
+    entities = makeMap(smoothness);
+
+    exit = new Exit([mapRows - 2, mapColumns - 2]);
+
+    entities[exit.hash] = exit;
+
+    return entities;
+}
+
+function placeEntranceExit(){
+    //TODO
+}
+
+class Node{
+    constructor(x, y, weight, distance=100000){
+        this.x = x;
+        this.y = y;
+        this.weight = weight;
+        this.distance = distance;
+    }
+}
+
+function entitiesToGraph(entityList){
+    graph = Array(mapRows).fill(0).map(x => Array(mapColumns).fill(0))
+    for(var e in entityList){
+        pos = entityList[e].position;
+        x = pos[0];
+        y = pos[1];
+
+        if(!graph[x][y]){
+            graph[x][y] = new Node(x, y, entityList[e].movementWeight);
+        }
+        else{
+            graph[x][y].weight = entityList[e].movementWeight + graph[x][y].weight;
+        }
+    }
+    return graph
+}
+
+function djikstra(entityMap, originX, originY){
+    //Djikstra's weighted pathfinding algorithm.
+    //NOTE: Algorithm requires ignoring border of map, meaning that origin cannot be 0
+    g = entitiesToGraph(entityMap);
+
+    currentNode = g[originX][originY]
+
+    currentNode.distance = 0;
+
+    unexplored = [currentNode,];
+
+    while(unexplored.length > 0){
+        //add neighbors of node to unexplored
+        currentNode = unexplored.pop();
+        x = currentNode.x;
+        y = currentNode.y;
+
+        neighbors = [];
+        numNeighbors = 0;
+        if(x > 0){
+            neighbors[numNeighbors] = (g[x-1][y]);
+            numNeighbors += 1;
+        }
+        if(x < mapRows-1){
+            neighbors[numNeighbors] = (g[x+1][y]);
+            numNeighbors += 1;
+        }
+        if(y > 0){
+            neighbors[numNeighbors] = (g[x][y-1]);
+            numNeighbors += 1;
+        }
+        if(y < mapColumns-1){
+            neighbors[numNeighbors] = (g[x][y+1]);
+            numNeighbors += 1;
+        }
+
+        for(i=0; i < numNeighbors; i++){
+            node = neighbors[i];
+            newDistance = currentNode.distance + node.weight;
+            if(node.distance > newDistance){
+                node.distance = newDistance;
+                unexplored.push(node);
+            }
+        }
+    }
+    return g;
+}
+
+
 class Entity{
     constructor(pos, movWeight, col, pri=0){
         this.position = pos;
         this.movementWeight = movWeight;
         this.colour = col;
         this.priority = pri;
-        this.hash = [pos, pri]
+        this.hash = [pos, pri];
+        this.distance = 100000;
     }
     get_position(){
         return this.position;
@@ -216,17 +327,24 @@ class Floor extends Entity{
 
 class Wall extends Entity{
     constructor(pos){
-        super(pos, 100, wallColour, 2)
+        super(pos, 1000, wallColour, 1)
+    }
+}
+
+class Exit extends Entity{
+    constructor(pos){
+        super(pos, 1, "orange", 3);
     }
 }
 
 
 //Main Script
-entities = new Map();
-entities = makeMap(entities, smoothness);
 
+entities = buildLevel(smoothness);
 drawEntities(entities)
 
-console.log(entities.size);
+
+djikstraWeights = djikstra(entities, mapRows-1, mapColumns-1)
+console.log(djikstraWeights);
 
 //setInterval(draw, 15);
